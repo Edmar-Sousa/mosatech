7487d80b43b1fe5e00564d050fab79dad223239e
Edmar_Sousa <edmarsousamendes4@gmail.com>
2021-05-23 20:00:59 -0300

    teste Router in server v4

--- core/classes/router.php
+++ core/classes/router.php
@@ -5,8 +5,15 @@ class Router {
     private const BASE_URL = __DIR__ . '/../views/';
 
     static function index($params) {
+        // print_r($params[2]);
 
-        print_r($params[1]);
+        $file_name = empty($params[2]) ? 'index' : $params[2];
+
+        if (file_exists(self::BASE_URL . $file_name . '.html'))
+            echo "ok";
+
+        else 
+            echo "nao ok";
     }
 }
 
