0658ce502187d5e788136c1f2a5bfc29e2427026
Edmar_Sousa <edmarsousamendes4@gmail.com>
2021-05-23 22:56:36 -0300

    template engine twig e refactory code

---
+++ core/route/detalhes.php
@@ -0,0 +1,11 @@
+<?php
+
+const BASE_URL_VIEWS = __DIR__ . '/../views';
+
+$loader = new \Twig\Loader\FilesystemLoader(BASE_URL_VIEWS);
+
+$twig = new \Twig\Environment($loader);
+
+echo $twig->render(
+    'detalhes.html', array()
+);
