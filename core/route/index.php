ec371993aabd0da33c4bbbe29f4657780c3a71ac
Edmar_Sousa <edmarsousamendes4@gmail.com>
2021-05-23 21:58:40 -0300

    Filess

--- core/route/index.php
+++ core/route/index.php
@@ -1,3 +1,11 @@
 <?php
 
-require_once __DIR__ . '/../views/index.html';
+const BASE_URL_VIEWS = __DIR__ . '/../views';
+
+$loader = new \Twig\Loader\FilesystemLoader(BASE_URL_VIEWS);
+
+$twig = new \Twig\Environment($loader);
+
+echo $twig->render(
+    'index.html', array('title' => 'ok')
+);
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'vendor/autoload.php';
+
 require_once 'core/classes/router.php';
 
 
